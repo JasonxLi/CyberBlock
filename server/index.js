e5b833917c963120a88b44cd43e28bdb56cd6bbb
JasonxLi <xin.6.li@ucdenver.edu>
2021-11-04 01:41:52 -0600

    dummy defenses

--- server/index.js
+++ server/index.js
@@ -38,22 +38,35 @@ io.on("connection", (socket) => {
             attack = "Attack 1: Lost my device";
         }
         else if (rolledNum === 2) {
-            attack = "Attack 2: Hacker hacking us";
+            attack = "Attack 2: Network compromised";
         }
         else if (rolledNum === 3) {
-            attack = "Attack 3: Total apocalypse";
+            attack = "Attack 3: Competitor Attack";
         }
         else if (rolledNum === 4) {
-            attack = "Attack 4: Cyborg invasion";
+            attack = "Attack 4: Supply Chain Attack";
         }
         else if (rolledNum === 5) {
-            attack = "Attack 5: CAT ATTACK";
+            attack = "Attack 5: Malware Attack";
         }
         else if (rolledNum === 6) {
-            attack = "Attack 6: Laser beam";
+            attack = "Attack 6: Cat Attack ლ(=ↀωↀ=)ლ";
         }
         console.log(`${attack}, ${lobbyId}`);
         io.in(lobbyId).emit("receive_roll", attack);
+    })
+
+    socket.on("start_buy_phase", (lobbyId) => {
+        var defenses = {
+            "Traceable Supply Chain" : 6, 
+            "Certified Suppliers" : 5,
+            "Data Encryption" : 8,
+            "Read-Only Software" : 8,
+            "Updatable Device" : 7,
+            "Two factor Authentication": 8,
+            "Connectivity Checker" : 6
+        }
+        io.in(lobbyId).emit("receive_defense_cards", defenses);
     })
 
     socket.on("disconnect", () => {
