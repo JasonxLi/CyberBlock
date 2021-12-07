2d3f64a872034ea48b6270b9e80c149fc193c4d9
JasonxLi <xin.6.li@ucdenver.edu>
2021-12-07 16:39:46 -0700

    heroku

--- client/src/context/ContextProvider.js
+++ client/src/context/ContextProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react';
 
 import io from 'socket.io-client'
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://cyberblock.herokuapp.com/");
 
 export const Context = createContext({});
 
